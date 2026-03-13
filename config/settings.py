7688688d172e0f50f16b6460129b80b4e0a8a8e1
Reina Yamamoto <reina.yamamoto@horizon-cg.com>
2026-03-13 22:33:32 +0900

    アクセス用設定を変更

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ API_URL = "https://api.openai.com/v1/images/generations"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 LOGGING = {
     'version': 1,

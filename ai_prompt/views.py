6284270e54b455bfc7f5bda1eff67dbca74acfc9
Reina Yamamoto <reina.yamamoto@horizon-cg.com>
2026-03-15 13:59:22 +0900

    進化画面作成

--- ai_prompt/views.py
+++ ai_prompt/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def evolution_view(request):
+    if request.method == "POST":
+        evolution = request.POST.get("evolution")
+        pass
+    return render(request, "ai_prompt/evolution.html")

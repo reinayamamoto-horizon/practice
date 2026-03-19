66c0eb4a62d0d57ee1f596eff5eb540f7fa4d8de
Reina Yamamoto <reina.yamamoto@horizon-cg.com>
2026-03-19 14:51:54 +0900

    アカウントを用意

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.db import IntegrityError
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import SignupForm
 
 # Create your views here.
+class Login (LoginView):
+    template_name = "accounts/login.html"
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        context["error"] = "ユーザー名またはパスワードが正しくありません。"
+        return self.render_to_response(context)
+
+
+class Signup(CreateView):
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        try:
+            self.object = form.save()
+            login(self.request, self.object)
+            return super().form_valid(form)
+        except IntegrityError:
+            form.add_error(None, "このメールアドレスは既に登録されています")
+            return self.form_invalid(form)
+
+
+
+class Logout(LogoutView):
+    template_name = "accounts/login.html"
+    #ログイン画面に遷移させる
+    success_url = reverse_lazy("login")

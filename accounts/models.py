98dc13049e5646710583a023e11cb4ac74117157
Reina Yamamoto <reina.yamamoto@horizon-cg.com>
2026-03-21 12:28:24 +0900

    カラム名の修正と冗長性の排除

--- accounts/models.py
+++ accounts/models.py
@@ -6,22 +6,21 @@ class User(AbstractUser):
     pass
 
 
-class Prompts(models.Model):
+class Prompt(models.Model):
     character = models.ForeignKey(
         "Character",
         on_delete=models.CASCADE
     )
-    styles = models.TextField(null=False)
-    seed = models.IntegerField(null=False)
-    model = models.TextField(null=False)
-    negative_prompt = models.TextField(null=False)
+    styles = models.TextField()
+    seed = models.IntegerField()
+    model = models.TextField()
+    negative_prompt = models.TextField()
+    class Meta:
+        unique_together = (("character",),)
+
 
 
 class Todo(models.Model):
-    user = models.ForeignKey(
-        "User",
-        on_delete=models.CASCADE
-    )
     character = models.ForeignKey(
         "Character",
         on_delete=models.CASCADE
@@ -31,28 +30,23 @@ class Todo(models.Model):
     body = models.TextField()
 
     display_flag = models.BooleanField(default=True)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     delete_flag = models.BooleanField(default=False)
 
 class Character(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE)
-
-    job = models.CharField(max_length=255)
-    level = models.IntegerField()
+    job = models.ForeignKey("Job", on_delete=models.CASCADE)
+    level = models.IntegerField(null=False)
     evolution = models.CharField(max_length=255)
-
     character_name = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    image_url =models.ImageField(upload_to='characters/', blank=True)
-    create_character = models.DateTimeField()
+    image_url = models.ImageField(upload_to='characters/', blank=True)
 
 class Job(models.Model):
     job_id = models.CharField(max_length=255)
     stage = models.IntegerField()
-    title = models.CharField(max_length=255)
-
     class Meta:
         unique_together = ("job_id", "stage")
 

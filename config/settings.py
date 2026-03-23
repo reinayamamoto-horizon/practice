cfa68b632f12834452b82ca00f4389716ad17d94
motonaga <mochi4714@gmail.com>
2026-03-23 17:10:27 +0900

    Lv表示とユーザー名表示作成

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard',
+    'dashboard.apps.DashboardConfig',
 ]
 
 MIDDLEWARE = [
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class EXP(models.Model):
+    level = models.IntegerField(null=False, verbose_name="Lv")
+    character_name = models.CharField(max_length=10, default="未設定", verbose_name="キャラクター名")
+    user_id = models.IntegerField(null=True,verbose_name="ユーザーID")
+    
+    
+    
+    
